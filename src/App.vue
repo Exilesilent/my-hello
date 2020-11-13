7ab7fa11d0221d5e4f4dc818c53ce5d0efaf7c78
kongxueming <kongxueming>
2020-11-13 17:46:22 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  img {
+    width: 200px;
+    height: 200px;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'lib-flexible'
+
 Vue.config.productionTip = false
 
 new Vue({
